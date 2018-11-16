03bfeaaffadcfcfb50c4d642303c177a3105f6ab
SCISYS\pocock_d <daniel.pocock@scisys.co.uk>
2018-11-16 15:45:00 +0000

    create react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './components/layout/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <NavBar></NavBar> 
+        </div>
+      </BrowserRouter> 
+
+    );
+  }
+}
+
+export default App;
