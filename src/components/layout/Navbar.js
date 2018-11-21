3408dd07910965bf49f5b82935fdd3a76212bdba
danppo <danielpocock@gmail.com>
2018-11-21 22:56:20 +0000

    completed user auth and details lession 29

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,18 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux'
 
-const NavBar = () => {
+
+const NavBar = (props) => {
+  const { auth, profile} = props;
+  // console.log(auth);
+  const links = auth.uid ? <SignedInLinks profile={profile} /> : <SignedOutLinks />
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">Mario Plan</Link>
-        <SignedInLinks></SignedInLinks>
-        <SignedOutLinks></SignedOutLinks>
+        { links }
       </div>
     </nav>
 
   )
 }
 
-export default NavBar
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
+  }
+}
+
+export default connect(mapStateToProps)(NavBar)
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -8,8 +8,8 @@ const ProjectList = ({projects}) => {
       {/* if we have projects then map through the projects */}
       { projects && projects.map(project => {
         return (
-          <Link to={'/project/' + project.id}>
-            <ProjectSummary project={project} key ={project.id} />
+          <Link to={'/project/' + project.id} key ={project.id}>
+            <ProjectSummary project={project}  />
           </Link>
         )
       })}
